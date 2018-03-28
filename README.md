8455519d63bb7b1a9ceaa4857da77ca5942f441e
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-03-28 14:31:06 +0700

    Update Readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Beautiful Log
 
-[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2Fphamhongphuc%2Flog.svg?type=shield)](https://app.fossa.io/projects/git%2Bgithub.com%2Fphamhongphuc%2Flog?ref=badge_shield)
+[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2Fphamhongphuc%2Flog.svg?type=shield)](https://app.fossa.io/projects/git%2Bgithub.com%2Fphamhongphuc%2Flog?ref=badge_shield) [![Travis Build](https://travis-ci.org/phamhongphuc/log.svg?branch=master)](https://travis-ci.org/phamhongphuc/log/builds "Most recent build") [![Average time to resolve an issue](http://isitmaintained.com/badge/resolution/phamhongphuc/log.svg)](https://github.com/phamhongphuc/log/issues "Average time to resolve an issue") [![Percentage of issues still open](http://isitmaintained.com/badge/open/phamhongphuc/log.svg)](https://github.com/phamhongphuc/log/issues "Percentage of issues still open")
 
 ## Description
 
@@ -17,11 +17,26 @@ npm install phamhongphuc/log
 
 ```javascript
 import log from 'log';
+import chalk from 'chalk';
+
 log({
-    title: {
-        a: 1,
-        b: 1,
-        c: 1
-    }
+    "repository": "phamhongphuc/log",
+    "author": "phamhongphuc",
+    "version": "0.2.2"
+}, {
+    title: "The most beautiful log",
+    color: chalk.blue,
+    length: 20,
+    middle: true,
 });
+```
+
+You will see:
+
+```log
+┏ The most beautiful log ━━━━━━━
+┃ repository : phamhongphuc/log
+┃     author : phamhongphuc
+┃    version : 0.2.2
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ```
--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,8 @@ const Edges = {
     vertical: '┃'
 };
 
+const max = (a: number, b: number) => (a > b ? a : b);
+
 export default function log(content: IContent, option: IOptions): void {
     option.length = option.length || 10;
     option.color = option.color || chalk.cyan;
@@ -24,9 +26,6 @@ export default function log(content: IContent, option: IOptions): void {
 
     let keyLength: number = 0;
     let valueLength: number = 0;
-
-    const max = (a: number, b: number) => (a > b ? a : b);
-    const maxLength: number = max(option.length, keyLength + valueLength) + 5;
 
     for (const key in content) {
         if (content.hasOwnProperty(key)) {
@@ -38,20 +37,23 @@ export default function log(content: IContent, option: IOptions): void {
             }
         }
     }
+    const maxLength: number = max(option.length, keyLength + valueLength + 6);
 
     const top = `${Edges.topLeft} ${option.title} `.padEnd(
-        maxLength + 5,
+        maxLength,
         Edges.horizontal
     );
     console.log(option.color(top));
     for (const key in content) {
         if (content.hasOwnProperty(key)) {
-            const keyString = key.padEnd(keyLength, ' ');
+            const keyString = option.middle
+                ? key.padStart(keyLength, ' ')
+                : key.padEnd(keyLength, ' ');
             const contentString = content[key].toString();
             const each = `${Edges.vertical} ${keyString} : ${contentString}`;
             console.log(option.color(each));
         }
     }
-    const bottom = Edges.bottomLeft.padEnd(maxLength + 5, Edges.horizontal);
+    const bottom = Edges.bottomLeft.padEnd(maxLength, Edges.horizontal);
     console.log(option.color(bottom));
 }
